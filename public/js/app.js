5dc20bebc5620eb3d3783d1cdcbb9c5b7a88a112
Maryna Aleksandrova <hello@marynaaleksandrova.info>
2012-10-19 21:05:09 +0200

    post msges to body

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,6 @@
 /*global cUnity: false, console: false, $: false, moment: false, _: false, page: false, async: false, alert: false, Showdown: false, EpicEditor: false, Github: false */
 $(function(){
+  $('.msg-area').focus();
   
   function showPage(pageName, pageTitle, fn){
     $('.page.visible').removeClass('visible').addClass('invisible');
@@ -11,5 +12,26 @@ $(function(){
       fn();
     }
   }
+
+  function postMsg(){
+    var msg = $('.msg-area').val();
+
+    $('.chat-body').append('<p class="msg-body">' + msg + '</p>');
+    $('.msg-area').val('');
+    $('.msg-area').focus();
+
+    $.post('https://api.github.com/repos/marynaaleksandrova/gitchat/issues/1/comments', JSON.stringify('issueData'), function(data) {
+
+    });
+  }
+
+  $("#msg-send").on("click", postMsg);
+  $('.msg-area').keypress(function(e) {
+  
+    if(e.keyCode == 13) {
+      e.preventDefault();
+      postMsg();
+    }
+  });
   
 });
