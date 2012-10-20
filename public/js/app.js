c2f112641df021fe02e3348bc796c05520bda61b
Anton Podviaznikov <podviaznikov@gmail.com>
2012-10-20 13:26:49 +0200

    error handling

--- public/js/app.js
+++ public/js/app.js
@@ -69,6 +69,9 @@ $(function(){
         }
         participants = _.uniq(participants, false, function(user){ return parseInt(user.id, 10);});
         renderParticipants(participants);
+      }).error(function(){
+        alert("Chat room doesn't exist");
+        chatContainer.spin(false);
       });
     }
 
